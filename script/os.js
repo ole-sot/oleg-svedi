443dff472e5884771c901f4721adad0b54a32a58
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-12-25 16:47:52 +0300

    Add sliders functionality

--- script/os.js
+++ script/os.js
@@ -1,4 +1,4 @@
-/*Player*/
+/*** Player ***/
 
 const audio = document.querySelector('audio');
 const playBtn = document.querySelector('.play-pause');
@@ -7,21 +7,17 @@ const btnNext = document.querySelector('.next-song');
 const artistName = document.querySelector('.artist-name');
 const songName = document.querySelector('.song-name');
 const cover = document.querySelector('.album-cover');
-const slider = document.querySelector('#line-for-time');
-const thumb = document.querySelector('.slider-thumb');
-const progress = document.querySelector('.progress');
-const faderLine = document.querySelector('#line-for-fader');
-const faderKnob = document.querySelector('.fader-knob');
 const timecode = document.querySelector('.timecode');
-
+const volumeFader = document.querySelector('#volume-fader');
 const timeline = document.querySelector('#player-timeline');
 
 
 let trackPlaying = false;
-let volumeMuted = false;
 let trackID = 0;
 
-/*Data*/
+
+/* --- Data --- */
+
 const audioFiles = [
     './audio/indie/01_leto.mp3',
     './audio/indie/02_rap.mp3',
@@ -43,14 +39,7 @@ const covers = [
     './img/album-covers/cover-1.png',
 ];
 
-
-function moveTimelineProgress() {
-    const timelineVal = timeline.value;
-
-    timeline.style.background = `linear-gradient(to right, var(--accent-samall-tr) ${timelineVal}%, var(--_track-color) ${timelineVal}%)`;
-}
-
-moveTimelineProgress();
+/* ------------ */
 
 
 playBtn.addEventListener('click', playTrack);
@@ -91,13 +80,11 @@ function loadTrack() {
     artistName.innerHTML = artists[trackID];
     songName.innerHTML = tracks[trackID];
     cover.src = covers[trackID];
-    progress.style.width = 0;
-    thumb.style.left = 0;
+    timeline.value = 0;
 }
 
 audio.addEventListener('loadeddata', () => {
-    // Set max value to slider
-    slider.setAttribute('max', audio.duration);
+    timeline.setAttribute('max', audio.duration);
 });
 
 loadTrack();
@@ -128,6 +115,19 @@ btnNext.addEventListener('click', nextTrack);
 
 audio.addEventListener('ended', nextTrack);
 
+function moveTimelineProgress() {
+    if (audio.currentTime > 0) {
+        const currentAudioTime = Math.floor(audio.currentTime);
+        const timePercentage = (currentAudioTime / audio.duration) * 100 + '%';
+    
+        timeline.style.background = `linear-gradient(to right, var(--accent-samall-tr) ${timePercentage}, var(--_track-color) ${timePercentage})`;
+    } else {
+        timeline.style.background = 'var(--_track-color)';
+    };
+}
+
+moveTimelineProgress();
+
 function setTime(output, input) {
     const min = Math.floor(input / 60);
     const sec = Math.floor(input % 60);
@@ -139,47 +139,32 @@ function setTime(output, input) {
     };
 }
 
-// setTime(fullTime, audio.duration);
-
 audio.addEventListener('timeupdate', () => {
     const currentAudioTime = Math.floor(audio.currentTime);
-    const timePercentage = (currentAudioTime / audio.duration) * 100 + '%';
 
     setTime(timecode, currentAudioTime);
-
-    progress.style.width = timePercentage;
-    thumb.style.left = timePercentage;
+    timeline.value = audio.currentTime;
+    moveTimelineProgress();
 });
 
 function handleSlider() {
-    const val = (slider.value / audio.duration) * 100 + '%';
-
-    progress.style.width = val;
-    thumb.style.left = val;
-
-    setTime(timecode, slider.value);
-
-    audio.currentTime = slider.value;
+    setTime(timecode, timeline.value);
+    audio.currentTime = timeline.value;
 }
 
 handleSlider();
 
-slider.addEventListener('input', handleSlider);
+timeline.addEventListener('input', handleSlider);
 
 let volVal;
 
 function respondToFader() {
-    // const volumeValue = (fader.style.bottom.replace('%', '0') / 10) / 50;
-    // audio.volume = volumeValue;
-    const maxVal = faderLine.getAttribute('max');
+    const maxVal = volumeFader.getAttribute('max');
 
-    volVal = (faderLine.value / maxVal) * 100 + '%';
-
-    faderKnob.style.bottom = volVal;
-
-    audio.volume = faderLine.value / 100;
+    volVal = (volumeFader.value / maxVal) * 100 + '%';
+    audio.volume = volumeFader.value / 100;
 }
 
 respondToFader();
 
-faderLine.addEventListener('input', respondToFader);
+volumeFader.addEventListener('input', respondToFader);
