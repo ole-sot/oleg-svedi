cb9061a8e9d1d18b622c8a448e948f8d8d5739b4
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-12-26 16:59:25 +0300

    Add functionality to genre switch buttons

--- script/os.js
+++ script/os.js
@@ -16,51 +16,106 @@ const genreBtns = document.querySelectorAll('.genre-selector button');
 let trackPlaying = false;
 let trackID = 0;
 let isToggled = true;
+let genre = 'indie';
 
 
 /* --- Data --- */
 
-const audioFiles = [
-    './audio/indie/01_leto.mp3',
-    './audio/indie/02_rap.mp3',
-    './audio/indie/03_toxic.mp3',
-];
-const tracks = [
-    'Лето',
-    'Рэп',
-    'Токсик',
-];
-const artists = [
-    'Телескоп',
-    'Телескоп',
-    'Телескоп',
-];
-const covers = [
-    './img/album-covers/cover-1.png',
-    './img/album-covers/cover-1.png',
-    './img/album-covers/cover-1.png',
-];
+const music = {
+    indie: {
+        audioFiles: [
+            './audio/indie/02-felicette-slows_down.mp3',
+            './audio/indie/01-moss_cape-prizrak.mp3',
+            './audio/indie/03-voodoo-doppelganger.mp3',
+        ],
+        tracks: [
+            'Slows Down',
+            'Призрак',
+            'Doppelganger',
+        ],
+        artists: [
+            'Felicette',
+            'Moss Cape',
+            'Вууду',
+        ],
+        covers: [
+            './img/album-covers/cover-1.png',
+            './img/album-covers/cover-1.png',
+            './img/album-covers/cover-1.png',
+        ],
+    },
+    metall: {
+        audioFiles: [
+            './audio/metall/01-the_revenge-grechka.mp3',
+            './audio/metall/02-anchar - cyberninja.mp3',
+            './audio/metall/03-lne-zamok.mp3',
+        ],
+        tracks: [
+            'Гречка',
+            'Киберниндзя',
+            'Замок',
+        ],
+        artists: [
+            'The Revenge',
+            'Anchar',
+            'Long Night Echoes',
+        ],
+        covers: [
+            './img/album-covers/cover-1.png',
+            './img/album-covers/cover-1.png',
+            './img/album-covers/cover-1.png',
+        ],
+    },
+    noiserock: {
+        audioFiles: [
+            './audio/noiserock/01-displaced_fuzz-track_4.mp3',
+            './audio/noiserock/02-nbs-noiserecsession-track_2.mp3',
+            './audio/noiserock/03-nbs-noiserecsession-track_3.mp3',
+        ],
+        tracks: [
+            'Трек 4',
+            'Трек 2',
+            'Трек 3',
+        ],
+        artists: [
+            'Displaced Fuzz',
+            'noisebleedsuns.',
+            'noisebleedsuns.',
+        ],
+        covers: [
+            './img/album-covers/cover-1.png',
+            './img/album-covers/cover-1.png',
+            './img/album-covers/cover-1.png',
+        ],
+    },
+    ambient: {
+        audioFiles: [
+            './audio/ambient/Forlate - Failing Better E1_2 M1_4.wav',
+            './audio/ambient/Forlate - The Turning E1_1 M1_4.wav',
+            './audio/ambient/Forlate - Rubber E1_5 M1_8.wav',
+        ],
+        tracks: [
+            'Failing Better',
+            'The Turning',
+            'Rubber',
+        ],
+        artists: [
+            'Forlate',
+            'Forlate',
+            'Forlate',
+        ],
+        covers: [
+            './img/album-covers/cover-1.png',
+            './img/album-covers/cover-1.png',
+            './img/album-covers/cover-1.png',
+        ],
+    },
+};
 
 /* ------------ */
 
 
-function clearActive() {
-    genreBtns.forEach(btn => {
-        if (btn.classList.contains('active') === true) {
-            btn.classList.remove('active');
-        };
-    });
-}
 
-function genreToggle() {
-    if (this.classList.contains('active') === false) {
-        clearActive();
-        this.classList.toggle('active');
-        // Change playlist
-    };
-}
-
-genreBtns.forEach(btn => btn.addEventListener('click', genreToggle));
 
 playBtn.addEventListener('click', playTrack);
 
@@ -90,16 +145,16 @@ function switchTrack() {
     };
 }
 
-const trackSrc = audioFiles[trackID];
+const trackSrc = music[genre].audioFiles[trackID];
 
 function loadTrack() {
-    audio.src = audioFiles[trackID];
+    audio.src = music[genre].audioFiles[trackID];
 
     audio.load();
 
-    artistName.innerHTML = artists[trackID];
-    songName.innerHTML = tracks[trackID];
-    cover.src = covers[trackID];
+    artistName.innerHTML = music[genre].artists[trackID];
+    songName.innerHTML = music[genre].tracks[trackID];
+    cover.src = music[genre].covers[trackID];
     timeline.value = 0;
 }
 
@@ -113,7 +168,7 @@ btnPrev.addEventListener('click', () => {
     trackID--;
 
     if (trackID < 0) {
-        trackID = audioFiles.length - 1;
+        trackID = music[genre].audioFiles.length - 1;
     };
 
     loadTrack();
@@ -123,7 +178,7 @@ btnPrev.addEventListener('click', () => {
 function nextTrack() {
     trackID++;
 
-    if (trackID > audioFiles.length - 1) {
+    if (trackID > music[genre].audioFiles.length - 1) {
         trackID = 0;
     };
 
@@ -137,8 +192,8 @@ audio.addEventListener('ended', nextTrack);
 
 function moveTimelineProgress() {
     if (audio.currentTime > 0) {
-        const currentAudioTime = Math.floor(audio.currentTime);
-        const timePercentage = (currentAudioTime / audio.duration) * 100 + '%';
+        const currentAudioTime = Math.round(audio.currentTime);
+        const timePercentage = Math.round((currentAudioTime / audio.duration) * 100) + '%';
     
         timeline.style.background = `linear-gradient(to right, var(--accent-samall-tr) ${timePercentage}, var(--_track-color) ${timePercentage})`;
     } else {
@@ -188,3 +243,41 @@ function respondToFader() {
 respondToFader();
 
 volumeFader.addEventListener('input', respondToFader);
+
+function clearActive() {
+    genreBtns.forEach(btn => {
+        if (btn.classList.contains('active') === true) {
+            btn.classList.remove('active');
+        };
+    });
+}
+
+function selectGenre(btn) {
+    switch (btn.classList[0]) {
+        case 'genre-1':
+            genre = 'indie';
+            break;
+        case 'genre-2':
+            genre = 'metall';
+            break;
+        case 'genre-3':
+            genre = 'noiserock';
+            break;
+        case 'genre-4':
+            genre = 'ambient';
+            break;
+    };
+}
+
+function switchGenre() {
+    if (this.classList.contains('active') === false) {
+        clearActive();
+        this.classList.toggle('active');
+        selectGenre(this);
+        trackID = 0;
+        loadTrack();
+        switchTrack();
+    };
+}
+
+genreBtns.forEach(btn => btn.addEventListener('click', switchGenre));
